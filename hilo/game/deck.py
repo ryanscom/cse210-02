22448a97902def900e4acc08bb4a47764f7045fe
ryanscom <ryanscom@byui.edu>
2022-05-06 12:49:05 -0700

    updated planning txt file, class py's

---
+++ hilo/game/deck.py
@@ -0,0 +1,9 @@
+
+
+class Deck:
+    """ Holds the card deck.  Deck = [cards 1 through 13]"""
+
+    """contructor here"""
+
+    def draw():
+        """draws a card from the deck.  value of 1-13 """
---
+++ hilo/game/director.py
@@ -0,0 +1,39 @@
+   
+
+
+class Director:
+
+                                    
+
+    """contructor here"""                                         
+    """points = 300"""                                           
+
+                                                            
+    def startgame():
+
+        get_inputs()
+            """returns guess, current card, next card"""
+
+
+        do_updates(guess, current_card, next_card)
+            
+        do_outputs(next card):
+
+    def get_inputs():
+        pass
+        # yes_no = input for picking a card (Yes or No)
+        # current card = draw()    draw card from deck class
+        # guess = input for guessing higher or lower
+        # next car = draw()  draw next card from deck class
+
+    def do_updates():
+        pass
+
+        # update points depending on if guessed correctly or not
+        # self.point += 100 or -=75
+
+    def do_outputs():
+        pass
+        # print what the next card was
+        # print wha points the player got or lost
+        # print the total player points
