34359a1a348c0c58f1eae1d06f4ca22bfd65472c
ryanscom <ryanscom@byui.edu>
2022-05-09 16:21:40 -0700

    Filled out class.pys. Now a working program.

--- hilo/game/deck.py
+++ hilo/game/deck.py
@@ -1,9 +1,42 @@
-
+import random
 
 class Deck:
-    """ Holds the card deck.  Deck = [cards 1 through 13]"""
+    """A deck of cards that can hold values between 1 and 13.
 
-    """contructor here"""
+    The responsibility of the Deck is to keep track of value of the card facing up. 
+   
+    Attributes:
+        value (int): number of the random card pulled from the deck
+    """
 
-    def draw():
-        """draws a card from the deck.  value of 1-13 """
+    def __init__(self):
+        """Constructs a new instance of Die with a value and points attribute.
+
+        Args:
+            self (Deck): An instance of Deck.
+        """
+        self.value = 0
+        self.suit = 0
+    
+    def draw(self):
+        """Generates a new random value and calculates the points.
+        
+        Args:
+            self (Deck): An instance of Deck.
+        """
+        suits = ['\u2666', '\u2665', '\u2663', '\u2660']
+
+        self.suit = random.choice(suits)
+        self.value = random.randint(1, 13)
+
+
+
+""" Notes on how to access a variable within a class method
+
+deck = Deck() # make an instance of Deck class
+deck.draw() # run the method in that instance
+card = deck.value # get the value of the variable wanted in the method 
+
+print(card) 
+
+"""
--- hilo/game/director.py
+++ hilo/game/director.py
@@ -1,39 +1,124 @@
-   
+from game.deck import Deck
 
 
-class Director:
+class Director():
+    """A person who directs the game. 
+    
+    The responsibility of a Director is to control the sequence of play.
 
-                                    
+    Attributes:
+        is_playing (boolean): Whether or not the game is being played.
+        score (int): The score for one round of play.
+        total_score (int): The score for the entire game.
+    """
 
-    """contructor here"""                                         
-    """points = 300"""                                           
+    def __init__(self):
+        """Constructs a new Director.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
 
-                                                            
-    def startgame():
-
-        get_inputs()
-            """returns guess, current card, next card"""
-
-
-        do_updates(guess, current_card, next_card)
+        self.is_playing = True
+        self.score = 300
+        self.right_wrong = "x"
+        self.current_card = 0
+        self.next_card = -1
+        self.current_suit = "a"
+        self.next_suit = "b"
+        self.guess = ""
+    
+    def start_game(self):
+        """Starts the game by running the main game loop.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        while self.is_playing:
             
-        do_outputs(next card):
+            self.get_inputs()
+            self.do_updates()
+            self.do_outputs()
 
-    def get_inputs():
-        pass
-        # yes_no = input for picking a card (Yes or No)
-        # current card = draw()    draw card from deck class
-        # guess = input for guessing higher or lower
-        # next car = draw()  draw next card from deck class
+    def get_inputs(self):
+        """Ask the user if they want to pick a card.
 
-    def do_updates():
-        pass
+        Args:
+            self (Director): An instance of Director.
+        """
 
-        # update points depending on if guessed correctly or not
-        # self.point += 100 or -=75
+        pick_card = input("pick card? [y/n] ")
+        self.is_playing = (pick_card == "y")
 
-    def do_outputs():
-        pass
-        # print what the next card was
-        # print wha points the player got or lost
-        # print the total player points
+        if self.current_card != self.next_card:
+            deck = Deck()
+            deck.draw()
+            card = deck.value
+            suit = deck.suit
+            self.current_card = card
+            self.current_suit = suit
+
+        print(f"The card is: {self.current_card}{self.current_suit}")
+
+        self.guess = input(f"Higher or lower? [h/l] ")
+
+        deck = Deck()
+        deck.draw()
+        card = deck.value
+        suit = deck.suit
+        self.next_card = card
+        self.next_suit = suit
+        
+
+    def do_updates(self):
+        """Updates the player's score.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        if not self.is_playing:
+            return 
+
+        if self.guess.lower() == "l":
+            if self.next_card > self.current_card:
+                self.score -= 75
+                self.right_wrong = "w"
+                
+            if self.next_card < self.current_card:
+                self.score += 100
+                self.right_wrong = "r"
+
+        if self.guess.lower() == "h":
+            if self.next_card < self.current_card:
+                self.score -= 75
+                self.right_wrong = "w"
+
+            if self.next_card > self.current_card:
+                self.score += 100
+                self.right_wrong = "r"
+        
+
+    def do_outputs(self):
+        """Displays the card and the score. Also asks the player if they want to pick another card. 
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        if not self.is_playing:
+            return
+        
+
+        print(f"The next card was: {self.next_card}{self.next_suit}")
+
+        if self.right_wrong == "r":
+            print(f"You gained 100 points!")
+        if self.right_wrong == "w":
+            print(f"You lost 75 points!")
+
+        print(f"You now have a total score of {self.score} points")
+        
+        self.current_card = self.next_card
+        self.current_suit = self.next_suit
+
+        self.is_playing == (self.score > 0)
+        
