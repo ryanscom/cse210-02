06507c6eff238c5ead5c974929efba5d908306e4
Eldon Martinez <mar18118@byui.edu>
2022-05-14 22:09:53 -0600

    fixing class name syntax

--- hilo/game/director.py
+++ hilo/game/director.py
@@ -2,7 +2,7 @@
 from game.deck import Deck
 
 
-class Director():
+class Director:
     """A person who directs the game. 
     
     The responsibility of a Director is to control the sequence of play.
